57faa483b2e9ca37bae42f48b471c24c7d3f57dd
GustavoDelfino <gustavo.delfino@ecommercebrasil.com.br>
2022-03-28 20:59:53 -0300

    01.Primeiros passos com PHP e HTML

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'Artigo.php';
+
+$artigo = new Artigo();
+
+$artigos = $artigo->exibirTodos();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach($artigos as $artigo) : ?>
+        <h2>
+            <a href=" <?php echo $artigo['link']; ?> ">
+                <?php echo $artigo['titulo']; ?>
+            </a>
+        </h2>
+        <p>
+            <?php echo $artigo['conteudo'];?>
+        </p>
+        <!-- O andforeach melhora legibilidade do codigo pois fica melhor para verificar onde o foreach está sendo fechado -->
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>

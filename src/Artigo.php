e3c9b2f61b55fc3d2253cd0d9b8a983893dd3199
GustavoDelfino <gustavo.delfino@ecommercebrasil.com.br>
2022-03-31 00:25:47 -0300

    03.Adicionando um artigo no banco

--- src/Artigo.php
+++ src/Artigo.php
@@ -9,6 +9,14 @@ class Artigo
     {
         $this->mysql = $mysql;
     }
+
+    public function adicionar(string $titulo, string $conteudo): void
+    {
+        $insereArtigo = $this->mysql->prepare('INSERT INTO artigos(titulo, conteudo) VALUES(?,?);');
+        $insereArtigo->bind_param('ss', $titulo, $conteudo);
+        $insereArtigo->execute();
+    }
+
     public function exibirTodos():array
     {
 
@@ -28,6 +36,7 @@ class Artigo
         $artigo = $selecionaArtigo->get_result()->fetch_assoc();
         return $artigo;
     }
+
 }
 
 ?>
